b14f2cb96f02e67a5270f4aa29cb7751ab1a61e0
Weizhe Chen <weizhechen@KKCompanydeMacBook-Air.local>
2023-05-25 04:36:36 +0800

    feat: Add checksum calculation function

--- cli/checksum.go
+++ cli/checksum.go
@@ -1,1 +1,81 @@
 package cli
+
+import (
+	"fmt"
+	algo "fops/algo"
+	filesys "fops/filesys"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var hashFunctions map[string]algo.Algorithm
+var checksum *cobra.Command
+
+func init() {
+
+	ChecksumCmd().Flags().StringP("file", "f", "", "File to operate on")
+
+	ChecksumCmd().Flags().Bool("md5", false, "checksum using md5")
+	ChecksumCmd().Flags().Bool("sha1", false, "checksum using sha1")
+	ChecksumCmd().Flags().Bool("sha256", false, "checksum using sha256")
+
+	ChecksumCmd().MarkFlagRequired("file")
+
+	hashFunctions = map[string]algo.Algorithm{
+		"md5":    algo.GetMD5(),
+		"sha1":   algo.GetSHA1(),
+		"sha256": algo.GetSHA256(),
+	}
+
+	rootcmd.AddCommand(ChecksumCmd())
+}
+
+func GetChecksum(value []byte, hashFn string) string {
+	return hashFunctions[hashFn].GetChecksum(value)
+}
+
+func ChecksumRun(cmd *cobra.Command, args []string) error {
+
+	filename, _ := cmd.Flags().GetString("file")
+	fs := filesys.File(filename).CheckFile()
+
+	if fs.Err != nil {
+		return fs.Err
+	} else {
+
+		algorithmFlag := ""
+		contents, _ := os.ReadFile(filename)
+
+		for _, algorithm := range hashFunctions {
+			match, err := cmd.Flags().GetBool(algorithm.GetType())
+			if err != nil {
+				return err
+			}
+			if match {
+				algorithmFlag = algorithm.GetType()
+			}
+		}
+
+		if algorithmFlag == "" {
+			return fmt.Errorf("checksum flag is missing. one of md5, sha1, sha256 is required")
+		}
+
+		fmt.Println(GetChecksum(contents, algorithmFlag))
+	}
+
+	return nil
+}
+
+func ChecksumCmd() *cobra.Command {
+	if checksum == nil {
+		checksum = &cobra.Command{
+			Use:   "checksum",
+			Short: "Generate checksum of a file",
+			Long: `Checksum retrieves the cryptographic hash of a file.
+		A valid text file and an algorithm flag (MD5, SHA1, SHA256) are needed.`,
+			RunE: ChecksumRun,
+		}
+	}
+	return checksum
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/gabriel-vasile/mimetype v1.4.2
 	github.com/spf13/cobra v1.7.0
+	github.com/stretchr/testify v1.8.3
 )
 
 require (
@@ -12,8 +13,6 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.3 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
